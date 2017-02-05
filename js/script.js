ac07eb74398033656a669f31afadd2c4cc927678
Sawood Alam <ibnesayeed@gmail.com>
2017-02-04 22:57:20 -0500

    Most of the paths are made absolute

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
 document.querySelector('#js-file .result').style.backgroundColor = '#f00';
-document.querySelector('#by-js-file .result').style.backgroundImage = "url('img/by-js-file.png')";
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('/acrts/img/by-js-file.png')";
