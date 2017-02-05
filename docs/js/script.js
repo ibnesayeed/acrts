67b03f6ad2c00b1b0e2a8091cf3c401c88dae28b
Sawood Alam <ibnesayeed@gmail.com>
2017-02-04 19:43:47 -0500

    Added real test cases

---
+++ docs/js/script.js
@@ -0,0 +1,2 @@
+document.querySelector('#js-file .result').style.backgroundColor = '#0f0';
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('img/by-js-file.png')";
---
+++ js/script.js
@@ -0,0 +1,2 @@
+document.querySelector('#js-file .result').style.backgroundColor = '#f00';
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('img/by-js-file.png')";
