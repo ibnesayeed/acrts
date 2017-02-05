aecbec1e18b6119427f09b5619fe9c419a7d9c0b
Sawood Alam <ibnesayeed@gmail.com>
2017-02-04 23:56:12 -0500

    Making references absolute URLs

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,2 +1,2 @@
 document.querySelector('#js-file .result').style.backgroundColor = '#0f0';
-document.querySelector('#by-js-file .result').style.backgroundImage = "url('/acrts/img/by-js-file.png')";
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('https://ibnesayeed.github.io/acrts/img/by-js-file.png')";
