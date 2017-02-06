9042c9d6f7c57495d4f88b7ed8894433effd5dbf
Sawood Alam <ibnesayeed@gmail.com>
2017-02-05 19:19:34 -0500

    Changed empty font style and refactored by-js-file reference

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 document.querySelector('#js-file .result').style.backgroundColor = '#f00';
-document.querySelector('#by-js-file .result').style.backgroundImage = "url('https://ibnesayeed.github.io/acrts/img/by-js-file.png')";
+var loc = 'https://ibnesayeed.github.io/acrts/img/by-js-file.png'
+document.querySelector('#by-js-file .result').style.backgroundImage = "url('" + loc + "')";
